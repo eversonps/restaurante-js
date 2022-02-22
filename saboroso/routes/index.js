ee2d544cc703fe242ad53cbaed771293007b95ba
Everson <eversonpereiraofc@gmail.com>
2022-02-22 13:59:16 -0300

    exibindo banco de dados com EJS

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+const connection = require('./../inc/db');
+var conn = require("./../inc/db")
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  conn.query("SELECT * FROM tb_menus ORDER BY title", (err, result)=>{
+    if(err){
+      console.error(err)
+    }else{
+      console.log(result)
+      res.render("index", {
+        title: 'Restaurante Saboroso',
+        menus: result
+      })
+    }
+  })
 });
 
 module.exports = router;
