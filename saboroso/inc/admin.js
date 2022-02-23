c59bdf9c5f2bd82db2c3e8d647bd1b5d311ae226
Everson <eversonpereiraofc@gmail.com>
2022-02-23 15:44:37 -0300

    organizando a administração em includes e adicionando links aos menus

---
+++ saboroso/inc/admin.js
@@ -0,0 +1,42 @@
+module.exports = {
+    getMenus(){
+        return [
+            {
+                text: "Tela Inicial",
+                href: "/",
+                icon: "home",
+                active: false
+            },
+            {
+                text: "Menu",
+                href: "/admin/menus",
+                icon: "cutlery",
+                active: false
+            },
+            {
+                text: "Reservas",
+                href: "/admin/reservations",
+                icon: "calendar-check-o",
+                active: false
+            },
+            {
+                text: "Contatos",
+                href: "/admin/contacts",
+                icon: "comments",
+                active: false
+            },
+            {
+                text: "Usuarios",
+                href: "/admin/users",
+                icon: "users",
+                active: false
+            },
+            {
+                text: "E-mails",
+                href: "/admin/emails",
+                icon: "envelope",
+                active: false
+            }
+        ]
+    }
+}
