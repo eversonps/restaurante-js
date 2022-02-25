e3dc1d1fac3d2b582bd6bb77e14fabb40cdb7792
Everson <eversonpereiraofc@gmail.com>
2022-02-25 14:23:16 -0300

    criando componente grid para refatorar o código

---
+++ saboroso/public/js/hcode-grid.js
@@ -0,0 +1,91 @@
+class HcodeGrid{
+    constructor(configs){
+
+      configs.listeners = Object.assign({ afterUpdateClick: (e)=>{
+        $("#modal-update").modal("show")
+      }}, configs.listeners)
+
+      this.options = Object.assign({}, {    
+        formCreate: "#modal-create form",
+        formUpdate: "#modal-update form",
+        btnUpdate: ".btn-update",
+        btnDelete: ".btn-delete",
+      }, configs)
+
+      this.initForms()
+      this.initButtons()
+    }
+
+    fireEvent(name, args){
+      if(typeof this.options.listeners[name] === "function"){
+        typeof this.options.listeners[name].apply(this, args)
+      }
+    }
+
+    initForms(){
+        this.formCreate = document.querySelector(this.options.formCreate)
+        this.formCreate.save().then(json=>{
+          window.location.reload()
+        }).catch(err=>{
+          console.error(err)
+        })
+      
+        this.formUpdate = document.querySelector(this.options.formUpdate);
+      
+        this.formUpdate.save().then(json=>{
+          window.location.reload()
+        }).catch(err=>{
+          console.error(err)
+        });
+    }
+
+    initButtons(){
+        [...document.querySelectorAll(this.options.btnDelete)].forEach(btn=>{
+          btn.addEventListener("click", e=>{
+            let tr = e.composedPath().find(el=>{
+              return (el.tagName.toUpperCase() === "TR")
+            })
+      
+            let data = JSON.parse(tr.dataset.row)
+      
+            if(confirm(eval("`" + this.options.deleteMsg + "`"))){
+              fetch(eval("`" + this.options.deleteUrl + "`"), {
+                  method: "DELETE"
+              }).then(response=>{
+                  response.json()
+              }).then(json=>{
+                  console.log(json)
+                  window.location.reload()
+              })
+            }
+          })
+        });
+      
+        [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn=>{   
+          btn.addEventListener("click", e=>{
+            this.fireEvent("beforeUpdateClick", [e])
+            let tr = e.composedPath().find(el=>{
+              return (el.tagName.toUpperCase() === "TR")
+            })
+      
+            console.log(tr)
+            let data = JSON.parse(tr.dataset.row)
+            console.log(data)
+      
+            for(let name in data){
+              var input = this.formUpdate.querySelector(`[name=${name}]`)
+              switch(name){
+                case "date":
+                  input.value = moment(data[name]).format("YYYY-MM-DD")
+                  break
+                default:
+                  if(input) input.value = data[name]
+                  break
+              }
+            }
+      
+            this.fireEvent("afterUpdateClick", [e])
+          })    
+        })
+    }
+}
